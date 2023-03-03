958a7215278ef1f66acf90f0c35d4e8fb3e34502
Rakshit Dubey <119107044+RakshitDubey@users.noreply.github.com>
2023-03-02 23:39:38 -0800

    Add files via upload

---
+++ third/details.py
@@ -0,0 +1,12 @@
+import phonenumbers
+from phonenumbers import timezone,geocoder,carrier
+
+number=input("Enter Your No. with +__:")
+phone=phonenumbers.parse(number)
+time=timezone.time_zones_for_number(phone)
+car=carrier.name_for_number(phone,"en")
+reg=geocoder.description_for_number(phone,"en")
+
+print(time)
+print(car)
+print(reg)
